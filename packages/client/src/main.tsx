487d704834e526bc4d3f72728ad2c189d2c3f982
xexex98 <xexex98@gmail.com>
2024-06-23 15:38:43 +0300

    YAN-42 eslint prettier husky import lint-staged and update packages

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import routes from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter(routes)
+import routes from './routes';
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);

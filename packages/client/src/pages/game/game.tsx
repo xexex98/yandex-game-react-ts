ac0c9e779fc85d23bfdee54614f59e6eefbc0353
apopov <pan@elkam.ru>
2024-06-27 10:08:09 +0500

    fix review

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -2,23 +2,27 @@ import { useCallback, useState } from 'react';
 
 import { HamsterCanvas } from './HamsterCanvas';
 import { StartScreen } from './StartScreen';
-  
+
 const Game = () => {
   const [countClick, setCountClick] = useState<number>(0);
   const [start, setStart] = useState(false);
-    
+
   const onClickCircle = useCallback(() => {
     setCountClick((prev) => prev + 1);
   }, []);
-    
+
   const changeStart = useCallback(() => {
-    setStart(true)
+    setStart(true);
   }, []);
 
   return (
     <>
       <div>Game: count click-{countClick}</div>
-      {start ? <HamsterCanvas onClickCircle={onClickCircle} /> : <StartScreen changeStart={changeStart}   
+      {start ? (
+        <HamsterCanvas onClickCircle={onClickCircle} />
+      ) : (
+        <StartScreen changeStart={changeStart} />
+      )}
     </>
   );
 };

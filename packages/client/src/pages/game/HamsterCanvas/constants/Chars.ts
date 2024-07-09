b3bb9bb7d40163a3ca46695f6e5e89cd6d3120da
andrey <kobzev605@gmail.com>
2024-07-10 00:57:53 +0300

    Добавил элемент для отрисовки символа

---
+++ packages/client/src/pages/game/HamsterCanvas/constants/Chars.ts
@@ -0,0 +1,29 @@
+export const chars = [
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'X',
+  'Z',
+  'C',
+  'B',
+  'O',
+  'L',
+  'J',
+];
+
+export const keyMap: Record<string, string> = {
+  KeyA: 'A',
+  KeyS: 'S',
+  KeyD: 'D',
+  KeyF: 'F',
+  KeyG: 'G',
+  KeyX: 'X',
+  KeyZ: 'Z',
+  KeyC: 'C',
+  KeyB: 'B',
+  KeyO: 'O',
+  KeyL: 'L',
+  KeyJ: 'J',
+};

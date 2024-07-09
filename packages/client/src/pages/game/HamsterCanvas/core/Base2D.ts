5848e7867e2c8f3e8fc54fb634306d6fae457935
andrey <kobzev605@gmail.com>
2024-07-10 00:55:27 +0300

    Добавил класс для элементов канваса

---
+++ packages/client/src/pages/game/HamsterCanvas/core/Base2D.ts
@@ -0,0 +1,54 @@
+import Render from './Render';
+
+export type optionsElement = {
+  x: number;
+  y: number;
+  width?: number;
+  height?: number;
+};
+
+export abstract class Base2D {
+  context: CanvasRenderingContext2D | null;
+  canvas: HTMLCanvasElement;
+
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+
+  private _show: boolean = true;
+
+  public get show() {
+    return this._show;
+  }
+
+  public set show(value: boolean) {
+    if (value === this._show || !this.context) {
+      return;
+    }
+    this._show = value;
+    if (value) {
+      Render.addElement(this, this.context);
+    } else {
+      Render.delElement(this, this.context);
+    }
+  }
+
+  constructor(canvas: HTMLCanvasElement, options: optionsElement) {
+    this.canvas = canvas;
+    this.context = canvas.getContext('2d');
+    this.x = options.x;
+    this.y = options.y;
+    this.width = options.width ?? 0;
+    this.height = options.height ?? 0;
+    if (this.context) {
+      Render.addElement(this, this.context);
+    }
+  }
+  abstract draw(): void;
+
+  setPosition(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}

c0b6c672db5a0394a34d27ef50153de446fda6b5
andrey <kobzev605@gmail.com>
2024-08-16 17:02:46 +0300

    feat: добавил логику для работы с ответами

---
+++ packages/server/app/controllers/reply.ts
@@ -0,0 +1,65 @@
+import { User } from '../decorators/types'
+import { Auth } from '../decorators/CheckAuth'
+import { Request, Response } from 'express'
+import { ReplyFromCommentDTO, ReplyFromReplyDTO } from '../dto/reply'
+import ReplyService from '../service/reply'
+
+class ReplyController {
+  user?: User
+
+  @Auth
+  async createFromComment(
+    req: Request<object, object, ReplyFromCommentDTO>,
+    res: Response
+  ) {
+    try {
+      if (this.user) {
+        const data = req.body
+        if (!data.commentId) {
+          throw new Error('Отсутствует поле commentId')
+        }
+        await ReplyService.createFromComment({
+          value: data.value,
+          commentId: data.commentId,
+          idUser: this.user.id,
+        })
+        res.status(201).send()
+      }
+    } catch (e) {
+      let message = ''
+      if (e instanceof Error) {
+        message = e.message
+      }
+      res.status(400).send(message)
+    }
+  }
+
+  @Auth
+  async createFromReply(
+    req: Request<object, object, ReplyFromReplyDTO>,
+    res: Response
+  ) {
+    try {
+      const data = req.body
+      if (!data.replyId) {
+        throw new Error('Отсутствует поле replyId')
+      }
+      if (this.user) {
+        await ReplyService.createFromReply({
+          value: data.value,
+          replyId: data.replyId,
+          idUser: this.user.id,
+        })
+        res.status(201).send()
+      }
+    } catch (e) {
+      let message = ''
+      if (e instanceof Error) {
+        message = e.message
+      }
+      res.status(400).send(message)
+    }
+  }
+}
+
+export const Reply = new ReplyController()

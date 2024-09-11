0455ebd79cd1c8d374392ddd4fc35d636cf34971
xexex98 <xexex98@gmail.com>
2024-09-11 11:07:39 +0300

    yandex

--- packages/client/src/consts/index.ts
+++ packages/client/src/consts/index.ts
@@ -6,7 +6,8 @@ export const APPLICATION_JSON = {
   'Content-Type': 'application/json;charset=utf-8',
 };
 export const API_URL = 'https://ya-praktikum.tech/api/v2';
-export const REDIRECT_URI = 'http://localhost:3000';
+export const REDIRECT_URI =
+  'https://ludocoders-ludoclicker-39.ya-praktikum.tech';
 export const OAUTH_URL =
   'https://oauth.yandex.ru/authorize?response_type=code&client_id=';
 export const COOKIE_IS_NOT_VALID = 'Cookie is not valid';
